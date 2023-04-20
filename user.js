ac13ece5b389ba2fc8809edd7986f4a840501f61
DinoDev <brankovicdino851@gmail.com>
2023-04-20 16:14:14 +0200

    Added to project 	package-lock.json 	package.json 	server.js 	views/

---
+++ user.js
@@ -0,0 +1,22 @@
+// dependencies
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+// connect to database
+mongoose.connect(
+  "mongodb+srv://dinob:<>@forumdb.oshly5z.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+// Create Model
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  username: String,
+  password: String,
+});
+// Export Model
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("userData", User, "userData");
