9ed9bb5e731c281a79a48f4ff3031e947243ae8d
DinoDev <brankovicdino851@gmail.com>
2023-04-26 16:30:27 +0200

    fixed server side issues + login and register work

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
-const connectEnsureLogin = require("connect-ensure-login");
-const User = require("./user.js");
-var router = express.Router();
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 
-app.use(express.static("views"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+app.use("/public/bilder/", express.static("./public/bilder"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
@@ -24,11 +30,15 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.html");
+  res.render("index");
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/views/login.html");
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.listen(port, () => {
