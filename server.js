1efc9e49bf3b28a93cf13afa924bf01365237e38
DinoDev <brankovicdino851@gmail.com>
2023-04-26 11:07:54 +0200

    Working on login + template engine

--- server.js
+++ server.js
@@ -5,20 +5,14 @@ const session = require("express-session");
 const passport = require("passport");
 const connectEnsureLogin = require("connect-ensure-login");
 const User = require("./user.js");
+var router = express.Router();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("views"));
+app.set("view engine", "ejs");
 
-app.use(
-  session({
-    secret: "r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60 * 60 * 1000 }, // 1 hour
-  })
-);
+app.use(express.static("views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
@@ -30,19 +24,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.render(__dirname + "/views/index.html");
 });
 
 app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/views/login.html");
-});
-
-app.get("/dashboard", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
-  res.send(`Hello ${req.user.username}. Your session ID is ${req.sessionID} 
-   and your session expires in ${req.session.cookie.maxAge} 
-   milliseconds.<br><br>
-   <a href="/logout">Log Out</a><br><br>
-   <a href="/secret">Members Only</a>`);
 });
 
 app.listen(port, () => {
